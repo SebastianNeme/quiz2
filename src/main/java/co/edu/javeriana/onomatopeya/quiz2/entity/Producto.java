8618f994636c4daf2e4b213f585cf2335d74cf67
SebastianNeme <s_peralta@javeriana.edu.co>
2026-09-07 17:06:36 -0500

    el producto entity Producto entity

--- src/main/java/co/edu/javeriana/onomatopeya/quiz2/entity/Producto.java
+++ src/main/java/co/edu/javeriana/onomatopeya/quiz2/entity/Producto.java
@@ -1,1 +1,32 @@
+package co.edu.javeriana.onomatopeya.quiz2.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table  (name = "productos")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String categoria;
+
+    @Column(nullable = false, precision = 12, scale = 2)
+    private int precio;
+
+    @Column(nullable = false)
+    private Boolean activo;
+}
