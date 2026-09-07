955485b120484d64a10aca00f3e6973aa3e53108
SebastianNeme <s_peralta@javeriana.edu.co>
2026-09-07 17:07:44 -0500

    Add ProductoResponseDto class with attributes

--- src/main/java/co/edu/javeriana/onomatopeya/quiz2/dto/response/ProductoResponseDto.java
+++ src/main/java/co/edu/javeriana/onomatopeya/quiz2/dto/response/ProductoResponseDto.java
@@ -1,1 +1,17 @@
+ package co.edu.javeriana.onomatopeya.quiz2.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+@AllArgsConstructor
+public class ProductoResponseDto {
+
+    private Long id;
+    private String nombre;
+    private String categoria;
+    private int precio;
+    private Boolean activo;
+}
