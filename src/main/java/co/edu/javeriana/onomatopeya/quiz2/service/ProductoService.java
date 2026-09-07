9c9a3ddb183f08e33a7da926790fff006246a39e
SebastianNeme <s_peralta@javeriana.edu.co>
2026-09-07 17:19:20 -0500

    Add Spring Boot application entry point

--- src/main/java/co/edu/javeriana/onomatopeya/quiz2/service/ProductoService.java
+++ src/main/java/co/edu/javeriana/onomatopeya/quiz2/service/ProductoService.java
@@ -1,1 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class Quiz2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run( Quiz2Application.class, args);
+	}
+
+}
