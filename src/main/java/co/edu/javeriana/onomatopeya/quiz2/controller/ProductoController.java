98b8e0de8cd1530ab7ed9a6b7ddd31c7614e99a3
SebastianNeme <s_peralta@javeriana.edu.co>
2026-09-07 17:16:29 -0500

    Implement ProductoController with CRUD operations

--- src/main/java/co/edu/javeriana/onomatopeya/quiz2/controller/ProductoController.java
+++ src/main/java/co/edu/javeriana/onomatopeya/quiz2/controller/ProductoController.java
@@ -1,1 +1,35 @@
 
+package co.edu.javeriana.onomatopeya.quiz2.controller;
+
+import co.edu.javeriana.onomatopeya.quiz2.dto.request.ProductoRequestDto;
+import co.edu.javeriana.onomatopeya.quiz2.dto.response.ProductoResponseDto;
+import co.edu.javeriana.onomatopeya.quiz2.service.ProductoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation     
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/productos")
+@RequiredArgsConstructor
+  
+public class ProductoController {
+
+    private final ProductoService productoService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductoResponseDto registrar(
+       @RequestBody ProductoRequestDto request) {
+
+        return productoService.registrar(request);
+    }
+
+    @GetMapping("/buscar/{categoriaa}")
+    public List<ProductoResponseDto> buscarPorCategoria(
+            @PathVariable("categoria") String categoria) {
+
+        return productoService.buscarPorCategoria(categoria);
+    }
+}
